cf8cf94d6ef0ccdb0f763bf7a7f6f5f5744b8fb2
chebdotnet <aleksandrs.rihliks@gmail.com>
2018-11-11 20:11:26 +0200

    added the 4th lesson

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@
  import './App.css';
 
  class App extends Component {
+   addTrack(){
+      console.log('add track', this.trackInput.value);
+      this.props.onAddTrack(this.trackInput.value);
+      this.trackInput.value = '';
+   }
+
    render() {
      console.log(this.props.testStore);
      return (
        <div className="App">
-         <input type="text" className="trackInput" />
-         <button className="addTrack">Add track</button>
+         <input type="text" className="trackInput" ref={(input) => {this.trackInput = input; }}/>
+         <button onClick={this.addTrack.bind(this)} className="addTrack">Add track</button>
          <ul className="list">
          {this.props.testStore.map((track, index) =>
            <li key={index}>{track}</li>
@@ -23,5 +29,9 @@ export default connect(
   state => ({
     testStore: state
   }),
-  dispatch => ({})
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({'type': 'ADD_TRACK', payload: trackName})
+    }
+  })
 )(App);
